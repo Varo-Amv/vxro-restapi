4184d4aaeff93a0f764b4f2fe155a209d798fbac
VXROO <72665810+Varo-Amv@users.noreply.github.com>
2022-03-01 15:36:36 +0000

    Add files via upload

---
+++ database/values.js
@@ -0,0 +1,19 @@
+/*
+ * Configurasi email
+ */
+exports.service = "gmail";
+exports.umail = "your-email@gmail.com";
+exports.upass = "YourPassword";
+
+/*
+ * Configurasi sending verification
+ */
+exports.subject_email = "VXRO WEB | Verification";
+
+/*
+ * Configurasi database
+ */
+exports.mongoUrl =
+  "mongodb+srv://Alvaro:varo123@cluster0.mn1td.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.dbGet = "vxro-api";
+exports.msgSuccesConnect = "Connected correctly to server";
